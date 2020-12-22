3b935edb6f6b83bd76a82103f7043d7abf770bb7
goelshivam1210 <goelshivam1210@gmail.com>
2020-12-22 01:15:50 -0500

    added plot functions and mac requirements.txt file

--- plot/plot.py
+++ plot/plot.py
@@ -1,4 +1,3 @@
-
 '''
 This code takes in the results file, 
 npy or csv and other formats and 
@@ -8,13 +7,11 @@ run this while training
 $ python plot.py -W <window_size> -P <pause_time(s)>
 
 For questions contact shivam.goel@tufts.edu
-
 '''
-# import glob
+
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
-import sys
 import seaborn as sns
 import argparse
 
@@ -30,6 +27,20 @@ args = vars(ap.parse_args())
 def moving_average(x, w):
     return np.convolve(x, np.ones(w), 'valid') / w
 
+# there are some duplicate and bad entries after 
+# 130K episodes so use this to remove them
+def clean_data(arr):
+    # this is HACK
+    for j in range(3):
+        for i in range(int(arr.shape[0])):
+            if (i > 100000 and i < 160000):
+                if int(arr[i][0]) < 2000:
+                    arr = np.delete(arr, (i), axis=0)
+
+    arr = np.delete(arr, (0), axis=1)
+    arr = arr.reshape(-1)
+    return arr
+
 def parser(filename):
 
     R = []
@@ -38,107 +49,109 @@ def parser(filename):
         reader = csv.reader(infile)
         for line in reader:
             if line:
+                ep_num = float(line[0])
                 reward = float(line[1])
                 # epsilon = float(line[2])
-                R.append(reward)
+                R.append((ep_num, reward))
+
                 # E.append(epsilon)
                 #print(line)
     R = np.array(R)
+    # print (R.shape, R[0], R[1])
     # E = np.array(E)
+    R = clean_data(R)
     R_sum = moving_average(R, args['window'])
     # E_avg = moving_average(E, args['window'])
     return R_sum
-
-    # all_data = []
-    # for name in glob.glob(directory): 
-    #     data = []
-    #     with open(name, mode = 'r') as infile:
-    #         reader = csv.reader(infile)
-    #         for line in reader:
-    #             reward = float(line[col])
-    #             data.append(reward)
-    #     # data = np.array(data)
-    #     all_data.append(np.asarray(data))
-    #     #print (all_data)
-    # all_data = np.asarray(all_data)
-    # # print (all_data.shape)
-    # return all_data
 
 if __name__ == '__main__':
 # domain v1
     exp_b_v1_file = 'data/train_results_1False.csv'
     exp_c_v1_file = 'data/train_results_1True.csv'
-    # exp_d_v1_file = 'data/train_results_1FalseClever.csv'
+    exp_d_v1_file = 'data/train_results_1Falseclever.csv'
+    exp_e_v1_file = 'data/train_results_1Trueclever.csv'
 
 # domain v2
     exp_b_v2_file = 'data/train_results_2False.csv'
     exp_c_v2_file = 'data/train_results_2True.csv'
-    # exp_d_v2_file = 'data/train_results_2FalseClever.csv'
+    exp_d_v2_file = 'data/train_results_2Falseclever.csv'
+    exp_e_v2_file = 'data/train_results_2Trueclever.csv'
 
 # domain v3
     exp_b_v3_file = 'data/train_results_3False.csv'
     exp_c_v3_file = 'data/train_results_3True.csv'
-    # exp_d_v3_file = 'data/train_results_3FalseClever.csv'
+    exp_d_v3_file = 'data/train_results_3Falseclever.csv'
+    exp_e_v3_file = 'data/train_results_3Trueclever.csv'
 
 # get all the data
     exp_b_v1 = parser(exp_b_v1_file)
     exp_c_v1 = parser(exp_c_v1_file)
-    # exp_d_v1 = parser(exp_d_v1_file)
+    exp_d_v1 = parser(exp_d_v1_file)
+    exp_e_v1 = parser(exp_e_v1_file)
 
     exp_b_v2 = parser(exp_b_v2_file)
     exp_c_v2 = parser(exp_c_v2_file)
-    # exp_d_v2 = parser(exp_d_v2_file)
+    exp_d_v2 = parser(exp_d_v2_file)
+    exp_e_v2 = parser(exp_e_v2_file)
 
     exp_b_v3 = parser(exp_b_v3_file)
     exp_c_v3 = parser(exp_c_v3_file)
-    # exp_d_v3 = parser(exp_d_v3_file)
+    exp_d_v3 = parser(exp_d_v3_file)
+    exp_e_v3 = parser(exp_e_v3_file)
+
 
     '''
     Nuisance Novelty Domain
     '''
-    plt.plot(exp_b_v1, label = 'Baseline')
-    plt.plot(exp_c_v1, label = 'Midline')
-    # plt.plot(exp_d_v1, label = 'Clever-Explorer')
+    plt.plot(exp_b_v1, label = 'Random weights')
+    plt.plot(exp_c_v1, label = 'Similar weights')
+    plt.plot(exp_d_v1, label = 'Clever-Explorer-Random')
+    plt.plot(exp_e_v1, label = 'Clever-Explorer-Similar')
+
     plt.xlabel("Number of episodes")
     plt.ylabel("Average cumulative reward per episode")
     plt.grid(True)
     plt.legend(loc = 7)
     # plt.yticks([0, 0.25, 0.5, 0.75, 1.0, 1.25])
-    plt.xlim(xmin=0.0, xmax=300000)
+    plt.xlim(xmin=0.0, xmax=310000)
     plt.tight_layout()
-    plt.savefig('nuisance_novelty_WS='+str(args['window'])+'.png', dpi = 600)
+    plt.savefig('nuisance_novelty_WS='+str(args['window'])+'.png', dpi = 800)
     plt.close()
     
     '''
     Shortcut Novelty Domain
     '''
-    plt.plot(exp_b_v2, label = 'Baseline')
-    plt.plot(exp_c_v2, label = 'Midline')
-    # plt.plot(exp_d_v2, label = 'Clever-Explorer')
+    plt.plot(exp_b_v2, label = 'Random weights')
+    plt.plot(exp_c_v2, label = 'Similar weights')
+    plt.plot(exp_d_v2, label = 'Clever-Explorer-Random')
+    plt.plot(exp_e_v2, label = 'Clever-Explorer-Similar')
+
     plt.xlabel("Number of episodes")
     plt.ylabel("Average cumulative reward per episode")
     plt.grid(True)
     plt.legend(loc = 7)
     # plt.yticks([0, 0.25, 0.5, 0.75, 1.0, 1.25])
-    plt.xlim(xmin=0.0, xmax=300000)
+    plt.xlim(xmin=0.0, xmax=310000)
     plt.tight_layout()
-    plt.savefig('shortcut_novelty_WS='+str(args['window'])+'.png', dpi = 600)
+    plt.savefig('shortcut_novelty_WS='+str(args['window'])+'.png', dpi = 800)
     plt.close()
 
     '''
     Catastrophic Novelty Domain
     '''
-    plt.plot(exp_b_v3, label = 'Baseline')
-    plt.plot(exp_c_v3, label = 'Midline')
-    # plt.plot(exp_d_v3, label = 'Clever-Explorer')
+    plt.plot(exp_b_v3, label = 'Random weights')
+    plt.plot(exp_c_v3, label = 'Similar weights')
+    plt.plot(exp_d_v3, label = 'Clever-Explorer-Random')
+    plt.plot(exp_e_v3, label = 'Clever-Explorer-Similar')
+
     plt.xlabel("Number of episodes")
     plt.ylabel("Average cumulative reward per episode")
     plt.grid(True)
     plt.legend(loc = 7)
     # plt.yticks([0, 0.25, 0.5, 0.75, 1.0, 1.25])
-    plt.xlim(xmin=0.0, xmax=300000)
+    plt.xlim(xmin=0.0, xmax=310000)
     plt.tight_layout()
-    plt.savefig('catastrophic_novelty_WS='+str(args['window'])+'.png', dpi = 600)
+    plt.savefig('catastrophic_novelty_WS='+str(args['window'])+'.png', dpi = 800)
 
     plt.close()
 
